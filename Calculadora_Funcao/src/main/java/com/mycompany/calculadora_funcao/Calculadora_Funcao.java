26e195eed00d1fa43e5cd264c761162ff5e7aa84
Jullyene Patta Ramos <jilesss9023Caju@outlook.com>
2024-05-25 01:11:37 -0300

    Construindo calculadora em java com Nathaly

---
+++ Calculadora_Funcao/src/main/java/com/mycompany/calculadora_funcao/Calculadora_Funcao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora_funcao;
+
+/**
+ *
+ * @author GAMER
+ */
+public class Calculadora_Funcao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
