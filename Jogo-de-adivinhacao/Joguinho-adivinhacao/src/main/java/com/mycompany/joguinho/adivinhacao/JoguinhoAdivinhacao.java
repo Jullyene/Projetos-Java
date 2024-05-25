20b46d753fdc41ae54d72b1a71169570f9fea3ca
Jullyene Patta Ramos <jilesss9023Caju@outlook.com>
2024-05-25 02:10:21 -0300

    Jogo de adivinhação pronto para subir ao github

--- Jogo-de-adivinhacao/Joguinho-adivinhacao/src/main/java/com/mycompany/joguinho/adivinhacao/JoguinhoAdivinhacao.java
+++ Jogo-de-adivinhacao/Joguinho-adivinhacao/src/main/java/com/mycompany/joguinho/adivinhacao/JoguinhoAdivinhacao.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.joguinho.adivinhacao;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author GAMER
@@ -11,6 +14,68 @@ package com.mycompany.joguinho.adivinhacao;
 public class JoguinhoAdivinhacao {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner resp_usu = new Scanner(System.in);
+        
+        Random rd = new Random();
+        
+        
+        boolean repetir = true;
+        int armazenar_resp_usu, maximo, minimo;
+        
+        minimo = 1;
+        maximo = 10;
+        
+        
+        int nume_randomico = rd.nextInt(maximo - minimo + 1) + minimo;
+        
+        
+        
+        
+        while(repetir){
+            
+            System.out.println("-------JOGO DE ADIVINHAÇÃO--------");
+            System.out.println("Qual será o número? [Digite um número de 1 até 10]:");
+            armazenar_resp_usu = resp_usu.nextInt();
+            
+            
+            
+            if(armazenar_resp_usu == nume_randomico){
+            System.out.println("Você a certou!");
+            repetir = false;
+        
+            }else{
+                
+                System.out.println("Você ainda não conseguiu adivinhar.Bora lá, tente novamente!");
+            }
+        
+            
+            
+            resp_usu.nextLine();
+            
+            System.out.println("Deseja continuar? (S- sim N- não): ");
+            String continuar_usuario = resp_usu.nextLine();
+            
+            
+            
+            //Transformar em letra maiúscula
+            String respostaUsuario = String.valueOf(continuar_usuario);
+            respostaUsuario = respostaUsuario.toUpperCase();
+            
+            
+            
+            
+            if (continuar_usuario.equalsIgnoreCase("S")) {
+            // Código se a resposta for "S"
+            } else if (continuar_usuario.equalsIgnoreCase("N")) {
+                repetir = false;
+            } else {
+            System.out.println("Resposta inválida. Por favor, digite s para continuar ou n para encerrar.");
+            }
+
+          
+        }
+        
+        
+        
     }
 }
